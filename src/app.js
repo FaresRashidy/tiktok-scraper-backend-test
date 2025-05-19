93b26d87e5ceab6d6b91434ae23f17eb82d3876a
faresrashidy <faresrashidy987@gmail.com>
2025-05-19 23:05:35 +0300

    CORS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import { setRoutes } from "./routes/index.js";
 
 const app = express();
+app.use(cors()); // Enable CORS for all origins
 app.use(express.json());
 
 setRoutes(app);
